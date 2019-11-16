e4c5204e4d611390af8cf1ead7b7fe11e9bdf519
Kishor6143 <57825339+Kishor6143@users.noreply.github.com>
2019-11-16 13:15:02 +0530

    tst project comit

---
+++ InheritanceProgram/src/Child.java
@@ -0,0 +1,43 @@
+ class Parent
+{
+	public void disp()
+	{
+		System.out.println("Parent class");
+		
+	}
+	public void add()
+	{
+	System.out.println("Add method in parent class");	
+	}
+
+}
+
+ public class Child extends Parent
+{
+
+	public void disp()
+	{
+		System.out.println("Child class");
+	
+	}
+	public void sub()
+	{
+		System.out.println("sub method in child class");
+	}
+	public static void main(String[] args)
+	{
+		Child ch = new Child();
+		ch.disp();
+		ch.add();
+		ch.sub();
+		Parent pr= new Child();
+		pr.add();
+        pr.disp();
+        Parent prch= new Parent();
+        prch.add();
+        prch.disp();
+
+
+	}
+
+}
